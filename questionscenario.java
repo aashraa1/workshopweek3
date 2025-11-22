eb873cf550e40802225929d6545a5b6ce8a2e7da
Aashra <aashrasingh444@gmail.com>
2025-11-22 20:46:12 +0545

    new changes in bluej

--- questionscenario.java
+++ questionscenario.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Write a description of class questionscenario here.
  *
- * @author (your name)
+ * @author (your aashra)
  * @version (a version number or a date)
  */
 public class questionscenario
